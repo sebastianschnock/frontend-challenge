57a3322268260690e111660b0cbf9d27031c6e19
sebastianschnock <sebastian.schnock@gmail.com>
2015-12-19 23:37:02 +0100

    Use a bit ES6

--- app/es6/main.js
+++ app/es6/main.js
@@ -2,13 +2,11 @@
 
 var wimdu = wimdu !== undefined ? wimdu : {};
 
-wimdu.init = (function () {
+wimdu.init = (() => {
 
-    var button = undefined,
-        extraInfo = undefined,
-        showExtraInfo = false;
+    let button, extraInfo, showExtraInfo = false;
 
-    return function (document) {
+    return document => {
 
         button = document.querySelector('.flat-description__show-more');
         extraInfo = document.querySelector('.extra-info');
@@ -16,13 +14,13 @@ wimdu.init = (function () {
         button.classList.toggle('hidden');
         extraInfo.classList.toggle('hidden');
 
-        button.addEventListener('click', function (event) {
+        button.addEventListener('click', event => {
             extraInfo.classList.add('slideable');
             extraInfo.classList.toggle('hidden');
             showExtraInfo = !showExtraInfo;
             button.innerHTML = showExtraInfo ? 'Show less' : 'Show more';
         });
-    };
+    }
 })();
 
 wimdu.init(document);
