785060085705a753c8e627dfe03c8090d5b428ed
sebastianschnock <sebastian.schnock@gmail.com>
2015-12-19 20:45:26 +0100

    Fix bug: extra info not shown with disabled js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,26 +2,26 @@
 
 var wimdu = wimdu !== undefined ? wimdu : {};
 
-wimdu.init = (function() {
+wimdu.init = (function () {
 
     var button;
     var extraInfo;
     var showExtraInfo = false;
 
-    return function(document) {
+    return function (document) {
 
         button = document.querySelector('.flat-description__show-more');
         extraInfo = document.querySelector('.extra-info');
 
         button.classList.toggle('hidden');
+        extraInfo.classList.toggle('hidden');
 
-        button.addEventListener('click', function(event) {
+        button.addEventListener('click', function (event) {
             extraInfo.classList.toggle('hidden');
             showExtraInfo = !showExtraInfo;
             button.innerHTML = showExtraInfo ? 'Show less' : 'Show more';
         });
-    }
-
+    };
 })();
 
 wimdu.init(document);
