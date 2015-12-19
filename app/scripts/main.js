7c42a37ae8f75c34793290200431bc5db3c3f16a
sebastianschnock <sebastian.schnock@gmail.com>
2015-12-19 16:30:45 +0100

    Add toggle functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('ready!');
+var wimdu = wimdu !== undefined ? wimdu : {};
+
+wimdu.init = (function() {
+
+    var button;
+    var extraInfo;
+    var showExtraInfo = false;
+
+    return function(document) {
+
+        button = document.querySelector('.flat-description__show-more');
+        extraInfo = document.querySelector('.extra-info');
+
+        button.classList.toggle('hidden');
+
+        button.addEventListener('click', function(event) {
+            extraInfo.classList.toggle('hidden');
+            showExtraInfo = !showExtraInfo;
+            button.innerHTML = showExtraInfo ? 'Show less' : 'Show more';
+        });
+    }
+
+})();
+
+wimdu.init(document);
